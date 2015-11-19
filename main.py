9537e3516f984552d61f4aee7ba22b4fe8d3da3d
antonbormotov <qqwrst@gmail.com>
2015-11-19 15:10:12 +0800

    Added CC dictionary with latest dates

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import re
 
 f = open('data.txt')
 
+cc = {
+    'hk': '',
+    'id': '',
+    'my': '',
+    'ph': '',
+    'sg': '',
+    'th': '',
+    'vn': ''
+    }
+
 # Retrieve latest import date
 for line in f:
     tmp = re.search('latest', line)
     if tmp:
-        latest_date = re.search('[0-9]{8}', line)
-        print "tmp: %s" % (latest_date.group(0))
-        break
-    #print vars(tmp)
-
-
+        current_index = re.search('product_([a-z]{2})_([0-9]{8})', line)
+        cc[current_index.group(1)] = current_index.group(2)
+        print "tmp: %s" % (cc[current_index.group(1)])
