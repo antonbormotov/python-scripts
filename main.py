dd51f414324f02820ffb04f7311537a220556db4
antonbormotov <qqwrst@gmail.com>
2015-11-19 17:21:05 +0800

    Fixed regex and removed extra print

--- main.py
+++ main.py
@@ -37,25 +37,20 @@ for line in f:
         cc_alias[current_index.group(1)].append(current_index.group(2))
         #print "alias tmp: %s" % (cc_alias[current_index.group(1)])
 
-#exit()
-
 with open('data.txt', 'r') as f:
     for line in f:
         for key in cc_latest:
-            tmp_regex = 'product_' + key + '_' + cc_latest[key][0] + '\s+[0-9]+\s[0-9]+(\s+[0-9]+)'
+            tmp_regex = 'product_' + key + '_' + cc_latest[key][0] + '\s+[0-9]+\s[0-9]+\s+([0-9]+)'
             tmp = re.search(tmp_regex, line)
-            print "tmp_regex: %s" % (tmp_regex)
             if tmp:
                 cc_latest[key].append(tmp.group(1))
-                print "tmp: %s - %s : %s" % (tmp.group(0), tmp.group(1), cc_latest[key])
 
         for key in cc_alias:
-            tmp_regex = 'product_' + key + '_' + cc_alias[key][0] + '\s+[0-9]+\s[0-9]+(\s[0-9]+)'
+            tmp_regex = 'product_' + key + '_' + cc_alias[key][0] + '\s+[0-9]+\s[0-9]+\s+([0-9]+)'
             tmp = re.search(tmp_regex, line)
             #print "tmp_regex: %s" % (tmp_regex)
             if tmp:
                 cc_alias[key].append(tmp.group(1))
-                #print "tmp: %s - %s : %s" % (tmp.group(0), tmp.group(1), cc_alias[key])
 
 print cc_latest
 print cc_alias
