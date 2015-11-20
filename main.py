8e174aadfa68e317ae43588038f755c0858a26ff
antonbormotov <qqwrst@gmail.com>
2015-11-20 14:55:04 +0800

    Converted stdout output to list

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+#!/usr/bin/env python
 import re
+import subprocess
 
-f = open('data.txt')
+try:
+    #output = subprocess.check_output(["./artisan es:list", ""], shell=True, stderr=subprocess.STDOUT)
+    with open("data.txt", "r") as f:
+        output_list = f.readlines()
+    #output_list = []
+    #i = 0
+    #for line in output.splitlines():
+    #    output_list.append(line.strip())
+    #    print "%d %s" % (i, output_list[i])
+    #    i += 1
+except Exception as e:
+    print "### Error: %s" % (str(e))
 
 cc_latest = {
     'hk': [],
@@ -22,7 +35,7 @@ cc_alias = {
     'vn': []
     }
 
-for line in f:
+for line in output_list:
     # Retrieve latest date for cc
     tmp = re.search('latest', line)
     if tmp:
@@ -37,20 +50,19 @@ for line in f:
         cc_alias[current_index.group(1)].append(current_index.group(2))
         #print "alias tmp: %s" % (cc_alias[current_index.group(1)])
 
-with open('data.txt', 'r') as f:
-    for line in f:
-        for key in cc_latest:
-            tmp_regex = 'product_' + key + '_' + cc_latest[key][0] + '\s+[0-9]+\s[0-9]+\s+([0-9]+)'
-            tmp = re.search(tmp_regex, line)
-            if tmp:
-                cc_latest[key].append(tmp.group(1))
+for line in output_list:
+    for key in cc_latest:
+        tmp_regex = 'product_' + key + '_' + cc_latest[key][0] + '\s+[0-9]+\s[0-9]+\s+([0-9]+)'
+        tmp = re.search(tmp_regex, line)
+        if tmp:
+            cc_latest[key].append(tmp.group(1))
 
-        for key in cc_alias:
-            tmp_regex = 'product_' + key + '_' + cc_alias[key][0] + '\s+[0-9]+\s[0-9]+\s+([0-9]+)'
-            tmp = re.search(tmp_regex, line)
-            #print "tmp_regex: %s" % (tmp_regex)
-            if tmp:
-                cc_alias[key].append(tmp.group(1))
+    for key in cc_alias:
+        tmp_regex = 'product_' + key + '_' + cc_alias[key][0] + '\s+[0-9]+\s[0-9]+\s+([0-9]+)'
+        tmp = re.search(tmp_regex, line)
+        #print "tmp_regex: %s" % (tmp_regex)
+        if tmp:
+            cc_alias[key].append(tmp.group(1))
 
 sum_alias = 0
 sum_latest = 0
